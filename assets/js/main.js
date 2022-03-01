6b646747b07403e0f6ae28169e353af56829ba02
Jonas Santos <jonas.santos9708@gmail.com>
2022-03-01 15:45:24 -0300

    menu-mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,19 @@ let abreMenu = document.querySelector('#abre_menu');
 let fechaMenu  = document.querySelector('#fecha_menu');
 let showMenu = document.querySelector('.nav-menu-mobile');
 
+let closeLinks = document.querySelectorAll('.close');
+console.log(closeLinks);
+
+for(let i = 0; i < closeLinks.length; i = i + 1 )
+    closeLinks[i].addEventListener('click',function(){
+        fecha_menu();
+    });
+
 function fecha_menu(){
     fechaMenu.style.display = 'none';
     showMenu.style.display = 'none';
-    abreMenu.style.display = 'block'
+    showMenu.style.transition = 'all 0.6s'
+    abreMenu.style.display = 'block';
 }
 
 function abre_menu(){
@@ -14,7 +23,7 @@ function abre_menu(){
     fechaMenu.style.display = 'block';
     showMenu.style.display = 'block';
     fechaMenu.style.color = '#202020';
-    
+
 
 }
 
